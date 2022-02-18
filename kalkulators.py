a1f61ba75d06d3a7bff95dc8c4c42037ead71237
RenaarsA <DogieLV@Gmail.com>
2022-02-18 14:04:33 +0200

    Update kalkulators.py

--- kalkulators.py
+++ kalkulators.py
@@ -1,6 +1,6 @@
 from tkinter import*
 mansLogs=Tk()
-mansLogs.title('Kalkulātors')
+mansLogs.title('Kalkulators')
 mansLogs.geometry('300x300')
 poga=Button(mansLogs,text='Sveiki! ', bg='green', fg='white')
 poga.pack()
