babb19ec5bb669df31feb75f9d47390be9b6c840
RenaarsA <DogieLV@Gmail.com>
2022-02-18 13:56:55 +0200

    Create kalkulators.py

---
+++ kalkulators.py
@@ -0,0 +1,7 @@
+from tkinter import*
+mansLogs=Tk()
+mansLogs.title('Kalkulātors')
+mansLogs.geometry('300x300')
+poga=Button(mansLogs,text='Sveiki! ', bg='green', fg='white')
+poga.pack()
+mansLogs.mainloop
